505a79f5180af42d5910f6e1c148398e2aa65729
janwes <hzwjanwes@outlook.com>
2021-08-01 22:44:20 +0800

    second commit

---
+++ estore-service/estore-service-search/src/main/java/com/estore/SearchApplication.java
@@ -0,0 +1,18 @@
+package com.estore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Janwes
+ * @version 1.0
+ * @package com.estore
+ * @date 2021/7/29 18:46
+ * @description
+ */
+@SpringBootApplication
+public class SearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class, args);
+    }
+}
